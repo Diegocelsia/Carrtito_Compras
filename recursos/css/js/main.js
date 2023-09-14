30c187721ba5dc7592db43ba8c43c8fe0be39236
Diego Suarez <suarezdiego297@gmail.com>
2023-09-14 16:08:00 -0500

    Carrito primer commit

---
+++ recursos/css/js/main.js
@@ -0,0 +1,9 @@
+import loader from "./components/loader.js";
+import showMenu from "./components/showMenu.js";
+import showCart from "./components/showCart.js";
+
+/*Ocultar Loader*/
+loader()
+showMenu()
+showCart()
+
