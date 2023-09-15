78b05b52148ee400e0fc926c492c71a28e8e9ec8
Diego Suarez <suarezdiego297@gmail.com>
2023-09-14 23:19:32 -0500

    desarollo final

--- recursos/css/js/main.js
+++ recursos/css/js/main.js
@@ -1,9 +1,14 @@
 import loader from "./components/loader.js";
 import showMenu from "./components/showMenu.js";
 import showCart from "./components/showCart.js";
+import products from "./components/products.js";
+import getProducts from "./helpers/getProducts.js";
+import cart from "./components/cart.js";
 
 /*Ocultar Loader*/
 loader()
 showMenu()
 showCart()
+const{ db, printProducts} = products(await getProducts())
+cart(db, printProducts)
 
